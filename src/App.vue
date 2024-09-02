0eff3ee183bad613cdfb4f27f3d3d1e7226ba89c
elyar <elyarnejati@gmail.com>
2024-09-02 20:21:09 +0330

    feat: add calculator component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
-<script setup></script>
+<script setup>
+import CalculatorComp from "@/components/CalculatorComp.vue";
+</script>
 
 <template>
-  <h1>Calcualtor</h1>
+  <calculator-comp></calculator-comp>
 </template>
 
-<style scoped></style>
+<style scoped>
+body {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/CalculatorComp.vue
@@ -0,0 +1,75 @@
+<script setup></script>
+
+<template>
+  <div class="calculator">
+    <div class="display">{{ current || "0" }}</div>
+    <div @click="clear" class="btn">C</div>
+    <div @click="sign" class="btn">+/-</div>
+    <div @click="percent" class="btn">%</div>
+    <div @click="divide" class="btn operator">÷</div>
+    <div @click="append('7')" class="btn">7</div>
+    <div @click="append('8')" class="btn">8</div>
+    <div @click="append('9')" class="btn">9</div>
+    <div @click="times" class="btn operator">X</div>
+    <div @click="append('4')" class="btn">4</div>
+    <div @click="append('5')" class="btn">5</div>
+    <div @click="append('6')" class="btn">6</div>
+    <div @click="minus" class="btn operator">-</div>
+    <div @click="append('1')" class="btn">1</div>
+    <div @click="append('2')" class="btn">2</div>
+    <div @click="append('3')" class="btn">3</div>
+    <div @click="add" class="btn operator">+</div>
+    <div @click="append('0')" class="btn zero">0</div>
+    <div @click="dot" class="btn">.</div>
+    <div @click="equal" class="btn operator">=</div>
+  </div>
+</template>
+
+<style scoped>
+.calculator {
+  margin: 0 auto;
+  width: 400px;
+  font-size: 40px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: minmax(50px, auto);
+}
+
+.display {
+  grid-column: 1 / 5;
+  background-color: #333;
+  color: white;
+  padding: 10px;
+}
+
+.zero {
+  grid-column: 1 / 3;
+}
+
+.btn {
+  background-color: #F2F2F2;
+  border: 1px solid #999;
+  text-align: center;
+  cursor: pointer;           
+  font-size: 1em;
+  transition: background-color 0.3s, box-shadow 0.3s, transform 0.3s;   
+}
+
+.btn:hover {
+  background-color: #e0e0e0;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  transform: scale(1.05);
+}
+
+.operator {
+  background-color: orange;
+  color: white;
+  transition: background-color 0.3s, box-shadow 0.3s, transform 0.3s;
+}
+
+.operator:hover {
+  background-color: #ff9500;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  transform: scale(1.05);
+}
+</style>
